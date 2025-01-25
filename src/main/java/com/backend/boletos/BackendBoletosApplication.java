48f6a7ccd863304f325cadd1139eacad73bbf487
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-01-24 19:54:37 -0600

    initial commit

---
+++ src/main/java/com/backend/boletos/BackendBoletosApplication.java
@@ -0,0 +1,13 @@
+package com.backend.boletos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBoletosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBoletosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/boletos/BackendBoletosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.boletos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBoletosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
