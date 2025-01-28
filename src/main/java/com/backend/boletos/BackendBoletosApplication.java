d096abfe28ece2ae94606a1c605a1f8f9ad88d94
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-01-28 15:27:10 -0600

    feat: add Thymeleaf dependency, create IndexController, and add index.html template

--- src/main/java/com/backend/boletos/BackendBoletosApplication.java
+++ src/main/java/com/backend/boletos/BackendBoletosApplication.java
@@ -1,17 +1,6 @@
 package com.backend.boletos;
-
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
-
-import com.backend.boletos.dtos.BoletoDto;
-import com.backend.boletos.service.BoletoService;
-
-
 
 
 @SpringBootApplication
@@ -21,23 +10,4 @@ public class BackendBoletosApplication {
 		SpringApplication.run(BackendBoletosApplication.class, args);
 	}
 }
-@Component
- class runner implements CommandLineRunner {
 
-	private final BoletoService boletoService;
-
-	public runner(BoletoService boletoService) {
-		this.boletoService = boletoService;
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		List<BoletoDto> boletos = Arrays.asList(new BoletoDto(300,1L,1L),
-												new BoletoDto(500,1L,1L),
-												new BoletoDto(800,1L,1L)
-		);
-		boletos.forEach(
-			b->boletoService.createBoleto(b)
-		);
-	}
-}
